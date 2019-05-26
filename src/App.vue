8b4b81a9ea184ee53242992f00e125bcc390503a
Will Aronow <john.aronow@gmail.com>
2019-05-25 22:04:22 -0700

    added fix for question not rendering next question

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <b-row>
         <b-col sm="6" offset="3">
           <QuestionBox
+            v-if="questions.length"
             :currentQuestion="questions[index]"
             :next="next"
           />
