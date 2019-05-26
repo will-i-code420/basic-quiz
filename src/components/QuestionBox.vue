e8c974d26d4fe7af97dc4a2d0a79029e4465646e
Will Aronow <john.aronow@gmail.com>
2019-05-25 17:28:28 -0700

    setup basic questionbox page with jumbrotron bootstrap

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Question Box</h1>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">
+        Question here
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        list of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next Question</b-button>
+    </b-jumbotron>
+  </div>
 </template>
