75a1040986c223aa64cf3dd35ce917a0cf303fc1
Will Aronow <john.aronow@gmail.com>
2019-05-25 17:14:24 -0700

    created basic header layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Header</h1>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><strong>Super Fun Quiz Time</strong></b-nav-item>
+      <b-nav-item disabled>Counter: 2/10</b-nav-item>
+    </b-nav>
+  </div>
 </template>
