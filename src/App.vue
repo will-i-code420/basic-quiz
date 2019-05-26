8f3b184e2f9633c743b88ce9401dd42bea706b12
Will Aronow <john.aronow@gmail.com>
2019-05-25 18:41:14 -0700

    added ability to display current question

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
-          <QuestionBox />
+          <QuestionBox
+            v-bind:question="questions[index]"
+          />
         </b-col>
       </b-row>
     </b-container>
@@ -24,7 +26,8 @@ export default {
   },
   data() {
     return {
-      questions: []
+      questions: [],
+      index: 0
     }
   },
   mounted: function() {
